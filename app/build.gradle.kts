9f520824fd9abe3ad4687f0172a8beeaf0a1ace7
mykhailo <speedcubing444@gmail.com>
2026-08-05 20:19:21 +0200

    Prepare release v1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.your_game_library"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
 
         // 2. Виправлено синтаксис buildConfigField
         buildConfigField("String", "IGDB_ID", "\"${localProperties.getProperty("IGDB_CLIENT_ID")}\"")
